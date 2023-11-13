0377e008c5ffd10e58b3428b665591a615e190a9
trebaul <97476970+Trebaul-NSI@users.noreply.github.com>
2023-11-13 21:49:06 +0100

    Deployed d606baa with MkDocs version: 1.4.1

---
+++ 1ere/seq4_b/TP_IDE.py
@@ -0,0 +1,9 @@
+def addition(a,b):
+    return a+b
+
+def soustraction(a,b):
+    return a-b
+
+valeur = 4
+somme = addition(valeur, valeur)
+difference = soustraction(somme, valeur)
---
+++ 1ere/seq4_b/TP_Portée.py
@@ -0,0 +1,8 @@
+def mystere(a):
+    p = 1
+    for i in range(a):
+        p = 2*p
+    return p
+
+a = 5
+p =  mystere(a)
